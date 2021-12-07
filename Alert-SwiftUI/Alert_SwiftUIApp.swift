d5adbb62177cb13f940a93515c422cd1e67443d5
Vadim <rvdagi05@yandex.ru>
2021-12-07 11:09:11 +0300

    Initial Commit

---
+++ Alert-SwiftUI/Alert_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alert_SwiftUIApp.swift
+//  Alert-SwiftUI
+//
+//  Created by cladendas on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Alert_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alert-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alert-SwiftUI
+//
+//  Created by cladendas on 07.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
