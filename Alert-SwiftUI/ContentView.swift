e213edd5e68cbca323aa7ef207c09746824bc59b
Vadim <rvdagi05@yandex.ru>
2021-12-07 12:26:50 +0300

    Добавил кнопку с Alert

--- Alert-SwiftUI/ContentView.swift
+++ Alert-SwiftUI/ContentView.swift
@@ -8,6 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    ///@State хранит состояние
+    @State var isError = false
+    
+    var body: some View {
+        
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world!")
+                .padding()
+        }
+        Button (action: {
+            self.isError = true
+            }, label: {
+                Text("Вход")
+            }).alert(isPresented: $isError, content: {
+                Alert(title: Text("TEST1"))
+                })
+
+
+    }
+}
+
+struct ContentViewQ: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +41,12 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        HStack {
+            
+            ContentView()
+            ContentViewQ()
+        }
+        
     }
 }
